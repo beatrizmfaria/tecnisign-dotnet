f56a3942d9f9eec3952fc89de63844c5c5fde225
Beatriz Faria <beatrizfaria@AirdeBeatriz2.lan>
2023-07-08 23:19:06 +0100

    first commit

---
+++ Atividade2_3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Atividade2_3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
